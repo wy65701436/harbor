065439da9d2c39e9035424800b0db0f21e31e359
Yan <wangyan_0219@hotmail.com>
2017-04-13 13:00:25 +0800

    add swagger checker (#4)
    * add swagger checker
    
    * update travis
    
    * upadte travis
    
    * update
    
    * update travis
    
    * update travis

---
+++ tests/swaggerchecker.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set +e
+
+if [ -z "$1" ]; then
+	echo '* Required input `git branch` not provided!'
+	exit 1
+fi
+
+SWAGGER_ONLINE_VALIDATOR="http://online.swagger.io/validator"
+HARBOR_SWAGGER_FILE="https://raw.githubusercontent.com/vmware/harbor/$1/docs/swagger.yaml"
+HARBOR_SWAGGER_VALIDATOR_URL="$SWAGGER_ONLINE_VALIDATOR/debug?url=$HARBOR_SWAGGER_FILE"
+echo $HARBOR_SWAGGER_FILE
+
+# Now try to ping swagger online validator, then to use it to do the validation.
+eval curl -f -I $SWAGGER_ONLINE_VALIDATOR
+curl_ping_res=$?
+if [ ${curl_ping_res} -eq 0 ]; then
+	echo "* cURL ping swagger validator returned success"
+else
+	echo "* cURL ping swagger validator returned an error (${curl_ping_res}), but don't fail the travis CI here."
+	exit 0
+fi
+
+# Use the swagger online validator to validate the harbor swagger file.
+eval curl -s $HARBOR_SWAGGER_VALIDATOR_URL > output.json
+curl_validate_res=$?
+validate_expected_results="{}"
+validate_actual_results=$(cat < output.json)
+
+if [ ${curl_validate_res} -eq 0 ]; then
+	if [ $validate_actual_results = $validate_expected_results ]; then
+		echo "* cURL check Harbor swagger file returned success"
+	else
+		echo "* cURL check Harbor swagger file returned an error ($validate_actual_results)"
+	fi
+else
+	echo "* cURL check Harbor swagger file returned an error (${curl_validate_res})"
+	exit ${curl_validate_res}
+fi
