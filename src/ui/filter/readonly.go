af6fa5e9af1b749299c789d175360a5ebdcbd451
wangyan <wangyan@vmware.com>
2018-03-21 04:04:29 -0700

    filterout delete repo

---
+++ src/ui/filter/readonly.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filter
+
+import (
+	"net/http"
+	"regexp"
+
+	"github.com/astaxie/beego/context"
+	"github.com/vmware/harbor/src/ui/config"
+)
+
+const (
+	repoURL = `/api/repositories`
+)
+
+//ReadonlyFilter filters the delete repo request and returns 503.
+func ReadonlyFilter(ctx *context.Context) {
+	filter(ctx.Request, ctx.ResponseWriter)
+}
+
+func filter(req *http.Request, resp http.ResponseWriter) {
+	if !config.ReadOnly() {
+		return
+	}
+	if req.Method == http.MethodDelete {
+		if matchRepoDelete(req) {
+			resp.WriteHeader(http.StatusServiceUnavailable)
+		}
+	}
+}
+
+func matchRepoDelete(req *http.Request) bool {
+	re := regexp.MustCompile(repoURL)
+	s := re.FindStringSubmatch(req.URL.Path)
+	if len(s) != 1 {
+		return false
+	}
+	return true
+}
