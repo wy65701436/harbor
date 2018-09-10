20a64d707892d7da8fa22adf29a58738a547d9b1
wang yan <wangyan@vmware.com>
2018-09-10 14:12:55 +0800

    fix path

--- tests/travis/e2e_run.sh
+++ tests/travis/e2e_run.sh
@@ -5,5 +5,5 @@ set -e
 IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
 echo $IP
 docker ps
-pybot -v ip:$IP -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/BAT.robot
+pybot -v ip:$IP -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/E2E.robot
 cat ./log.html
