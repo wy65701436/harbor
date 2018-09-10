87782cf49e9ae32c61a50133c5bd0b2fa16e0dd0
wang yan <wangyan@vmware.com>
2018-09-10 14:10:43 +0800

    fix issue

--- tests/travis/e2e_install.sh
+++ tests/travis/e2e_install.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-docker pull vmware/harbor-e2e-engine:1.41
-sudo make install GOBUILDIMAGE=golang:1.9.2 COMPILETAG=compile_golangimage CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:1.6.0
+sudo apt-get update && sudo apt-get install -y python-dev openjdk-7-jdk
+sudo wget https://bootstrap.pypa.io/get-pip.py && python ./get-pip.py && pip install pyasn1 google-apitools==0.5.15 gsutil robotframework robotframework-sshlibrary robotframework-httplibrary requests dbbot robotframework-selenium2library robotframework-pabot --upgrade
+sudo make install GOBUILDIMAGE=golang:1.9.2 COMPILETAG=compile_golangimage CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:1.6.0 NOTARYFLAG=true CLAIRFLAG=true
