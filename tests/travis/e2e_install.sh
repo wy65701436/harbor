ac2c6c0ad6cd2d7f706603c98c9be7e995d91e41
wang yan <wangyan@vmware.com>
2018-09-10 14:21:37 +0800

    fix issue

--- tests/travis/e2e_install.sh
+++ tests/travis/e2e_install.sh
@@ -3,5 +3,5 @@
 set -e
 
 sudo apt-get update && sudo apt-get install -y python-dev openjdk-7-jdk
-sudo wget https://bootstrap.pypa.io/get-pip.py && python ./get-pip.py && pip install pyasn1 google-apitools==0.5.15 gsutil robotframework robotframework-sshlibrary robotframework-httplibrary requests dbbot robotframework-selenium2library robotframework-pabot --upgrade
+sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python ./get-pip.py && sudo pip install pyasn1 google-apitools==0.5.15 gsutil robotframework robotframework-sshlibrary robotframework-httplibrary requests dbbot robotframework-selenium2library robotframework-pabot --upgrade
 sudo make install GOBUILDIMAGE=golang:1.9.2 COMPILETAG=compile_golangimage CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:1.6.0 NOTARYFLAG=true CLAIRFLAG=true
--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -5,9 +5,11 @@ set -e
 sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
 cat ./src/ui_ng/npm-ut-test-results
 sudo docker-compose -f ./make/docker-compose.test.yml up -d
+sleep 10
 #sudo make go_check
 ./tests/pushimage.sh
 docker ps
+cat /var/log/harbor/adminserver.log
 go test -race -i ./src/ui ./src/adminserver ./src/jobservice
 sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
 goveralls -coverprofile=profile.cov -service=travis-ci
