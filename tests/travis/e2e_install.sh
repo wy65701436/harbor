0d1315827402517561c01327f617daa09fc5175f
wang yan <wangyan@vmware.com>
2018-09-10 14:57:24 +0800

    fix issue

--- tests/travis/e2e_install.sh
+++ tests/travis/e2e_install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo apt-get update && sudo apt-get install -y python-dev openjdk-7-jdk
+sudo apt-get update && sudo apt-get install -y --no-install-recommends python-dev openjdk-7-jdk libssl-dev && sudo apt-get autoremove -y && sudo rm -rf /var/lib/apt/lists/*
 sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python ./get-pip.py && sudo pip install robotframework robotframework-httplibrary requests dbbot robotframework-pabot --upgrade
 sudo make install GOBUILDIMAGE=golang:1.9.2 COMPILETAG=compile_golangimage CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:1.6.0 NOTARYFLAG=true CLAIRFLAG=true
