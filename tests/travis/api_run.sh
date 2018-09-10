476f8c48c785197ee98c0a5303a4341881b141b5
wang yan <wangyan@vmware.com>
2018-09-10 18:37:49 +0800

    fix issue

--- tests/travis/api_run.sh
+++ tests/travis/api_run.sh
@@ -6,4 +6,4 @@ IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
 echo $IP
 docker ps
 pybot -v ip:$IP -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/API.robot
-cat ./log.html
+cat /home/travis/gopath/src/github.com/goharbor/harbor/log.html
