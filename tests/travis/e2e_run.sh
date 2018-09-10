59a56afa18444692c817b8371ac0dbfe614dde2a
wang yan <wangyan@vmware.com>
2018-09-10 10:59:28 +0800

    fix issue

--- tests/travis/e2e_run.sh
+++ tests/travis/e2e_run.sh
@@ -3,4 +3,8 @@
 set -e
 
 IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
-docker run -i --privileged -v /home/travis/gopath/src/github.com/goharbor/harbor:/travis -v /harbor/ca:/ca -w /drone vmware/harbor-e2e-engine:1.41 pybot -v ip:$IP -v notaryServerEndpoint:$IP:4443 -v ip1: -v HARBOR_PASSWORD:Harbor12345 /travis/tests/robot-cases/Group0-BAT/BAT.robot
+echo $IP
+docker ps
+docker run -i --privileged -v /home/travis/gopath/src/github.com/goharbor/harbor:/drone -v /harbor/ca:/ca -w /drone vmware/harbor-e2e-engine:1.41 pybot -v ip:$IP -v notaryServerEndpoint:$IP:4443 -v ip1: -v HARBOR_PASSWORD:Harbor12345 /drone/tests/robot-cases/Group0-BAT/BAT.robot
+
+cat ./log.html
--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -5,8 +5,9 @@ set -e
 sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
 cat ./src/ui_ng/npm-ut-test-results
 sudo docker-compose -f ./make/docker-compose.test.yml up -d
-sudo make go_check | true
+#sudo make go_check
 ./tests/pushimage.sh
+docker ps
 go test -race -i ./src/ui ./src/adminserver ./src/jobservice
 sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
 goveralls -coverprofile=profile.cov -service=travis-ci
