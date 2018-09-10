0f8938ab5d3dbf84635574bc221727664f3cf741
wang yan <wangyan@vmware.com>
2018-09-10 10:30:10 +0800

    fix travis issue

--- tests/travis/ut_install.sh
+++ tests/travis/ut_install.sh
@@ -14,3 +14,14 @@ go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
 go get -u github.com/client9/misspell/cmd/misspell
 sudo service postgresql stop
+
+IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
+export POSTGRESQL_HOST=$IP
+export REGISTRY_URL=$IP:5000
+
+cd tests && sudo ./ldapprepare.sh && sudo ./admiral.sh && cd ..
+sudo make compile_adminserver
+sudo make -f make/photon/Makefile _build_adminserver _build_db _build_registry -e VERSIONTAG=dev -e CLAIRDBVERSION=dev -e REGISTRYVERSION=${REG_VERSION}
+sudo sed -i 's/__reg_version__/${REG_VERSION}-dev/g' ./make/docker-compose.test.yml
+sudo sed -i 's/__version__/dev/g' ./make/docker-compose.test.yml
+sudo mkdir -p ./make/common/config/registry/ && sudo mv ./tests/reg_config.yml ./make/common/config/registry/config.yml
--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -5,8 +5,8 @@ set -e
 sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
 cat ./src/ui_ng/npm-ut-test-results
 sudo docker-compose -f ./make/docker-compose.test.yml up -d
-make go_check
+sudo make go_check | true
 ./tests/pushimage.sh
-#go test -race -i ./src/ui ./src/adminserver ./src/jobservice
-#sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
-#goveralls -coverprofile=profile.cov -service=travis-ci
+go test -race -i ./src/ui ./src/adminserver ./src/jobservice
+sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
+goveralls -coverprofile=profile.cov -service=travis-ci
