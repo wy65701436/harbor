fc06c85dfe29a9c4c6a67566c3fedc7926d335e9
wang yan <wangyan@vmware.com>
2018-09-12 16:41:39 +0800

    fix issues

--- tests/travis/api_run.sh
+++ tests/travis/api_run.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
 docker ps
-
 # run db auth api cases
 if [ "$1" = 'DB' ]; then
-    pybot -v ip:$IP -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/API_DB.robot
+    pybot -v ip:$2 -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/API_DB.robot
 fi
 # run ldap api cases
 if [ "$1" = 'LDAP' ]; then
-    pybot -v ip:$IP -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/API_LDAP.robot
+    pybot -v ip:$2 -v HARBOR_PASSWORD:Harbor12345 /home/travis/gopath/src/github.com/goharbor/harbor/tests/robot-cases/Group0-BAT/API_LDAP.robot
 fi
+
 #cat /home/travis/gopath/src/github.com/goharbor/harbor/log.html
 #ls -la /var/log/harbor
--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -2,18 +2,17 @@
 
 set -e
 
+export POSTGRESQL_HOST=$1
+export REGISTRY_URL=$1:5000
+
 sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
 cat ./src/ui_ng/npm-ut-test-results
 sudo docker-compose -f ./make/docker-compose.test.yml up -d
 sleep 10
-#sudo make go_check
+sudo make go_check
 ./tests/pushimage.sh
 docker ps
+
 go test -race -i ./src/ui ./src/adminserver ./src/jobservice
-
-IP=`ip addr s eth0 |grep "inet "|awk '{print $2}' |awk -F "/" '{print $1}'`
-export POSTGRESQL_HOST=$IP
-export REGISTRY_URL=$IP:5000
-
 sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
 goveralls -coverprofile=profile.cov -service=travis-ci
