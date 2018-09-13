1e96fff6c1534cd51be4a85a2a448962f357858c
wang yan <wangyan@vmware.com>
2018-09-13 15:18:17 +0800

    fix travis issue

--- tests/travis/ut_run.sh
+++ tests/travis/ut_run.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-export GOPATH=/home/travis/gopath
-sudo make go_check
-#export POSTGRESQL_HOST=$1
-#export REGISTRY_URL=$1:5000
-#
-#sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
-#cat ./src/ui_ng/npm-ut-test-results
-#sudo docker-compose -f ./make/docker-compose.test.yml up -d
-#sleep 10
-#./tests/pushimage.sh
-#docker ps
-#
-#go test -race -i ./src/ui ./src/adminserver ./src/jobservice
-#sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
-#goveralls -coverprofile=profile.cov -service=travis-ci
+export POSTGRESQL_HOST=$1
+export REGISTRY_URL=$1:5000
+
+sudo make run_clarity_ut CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:${UI_BUILDER_VERSION}
+cat ./src/ui_ng/npm-ut-test-results
+sudo docker-compose -f ./make/docker-compose.test.yml up -d
+sleep 10
+./tests/pushimage.sh
+docker ps
+
+go test -race -i ./src/ui ./src/adminserver ./src/jobservice
+sudo -E env "PATH=$PATH" "POSTGRES_MIGRATION_SCRIPTS_PATH=/home/travis/gopath/src/github.com/goharbor/harbor/make/migrations/postgresql/" ./tests/coverage4gotest.sh
+goveralls -coverprofile=profile.cov -service=travis-ci
