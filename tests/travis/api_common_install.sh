48413fd0cb6988e29943e27dde3f96150681c045
wang yan <wangyan@vmware.com>
2018-09-24 23:59:49 +0800

    fix issues

--- tests/travis/api_common_install.sh
+++ tests/travis/api_common_install.sh
@@ -20,7 +20,7 @@ if [ "$2" = 'DB' ]; then
 fi
 
 sudo apt-get update && sudo apt-get install -y --no-install-recommends python-dev openjdk-7-jdk libssl-dev && sudo apt-get autoremove -y && sudo rm -rf /var/lib/apt/lists/*
-sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python ./get-pip.py && sudo pip install --ignore-installed urllib3 chardet requests && sudo pip install pyasn1 google-apitools==0.5.15 gsutil==4.28 robotframework robotframework-httplibrary requests dbbot robotframework-pabot --upgrade
+sudo wget https://bootstrap.pypa.io/get-pip.py && sudo python ./get-pip.py && sudo pip install --ignore-installed urllib3 chardet requests six && sudo pip install pyasn1 google-apitools==0.5.15 gsutil==4.28 robotframework robotframework-httplibrary requests dbbot robotframework-pabot --upgrade
 sudo make swagger_client
 sudo make install GOBUILDIMAGE=golang:1.9.2 COMPILETAG=compile_golangimage CLARITYIMAGE=goharbor/harbor-clarity-ui-builder:1.6.0 NOTARYFLAG=true CLAIRFLAG=true CHARTFLAG=true
 sleep 10
