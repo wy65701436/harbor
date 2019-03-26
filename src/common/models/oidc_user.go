d3cda9f39c321936faf5e800ba341ecd54ff6853
wang yan <wangyan@vmware.com>
2019-03-26 19:14:13 +0800

    fix issue

--- src/common/models/oidc_user.go
+++ src/common/models/oidc_user.go
@@ -9,7 +9,7 @@ type OIDCUser struct {
 	ID           int64     `orm:"pk;auto;column(id)" json:"id"`
 	UserID       int       `orm:"column(user_id)" json:"user_id"`
 	Secret       string    `orm:"column(secret)" json:"secret"`
-	Sub          string    `orm:"column(Sub)" json:"sub"`
+	Sub          string    `orm:"column(sub)" json:"sub"`
 	CreationTime time.Time `orm:"column(creation_time);auto_now_add" json:"creation_time"`
 	UpdateTime   time.Time `orm:"column(update_time);auto_now" json:"update_time"`
 }
