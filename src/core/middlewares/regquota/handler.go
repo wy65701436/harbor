a1edc0173ff97e330ce9ffc0a5f7355055a2df28
wang yan <wangyan@vmware.com>
2019-06-14 18:41:19 +0800

    fix issues

--- src/core/middlewares/regquota/handler.go
+++ src/core/middlewares/regquota/handler.go
@@ -49,8 +49,12 @@ func (bh regQuotaHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 			log.Infof("manifest digest... %s", imageDigest)
 			log.Infof("manifest size... %v", imageSize)
 			req.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+			return
 		}
 	}
+
+	http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to Unmarshal Manifest %v", err)), http.StatusInternalServerError)
+	return
 
 	bh.next.ServeHTTP(rw, req)
 }
