a5845b681aded90fc9f0a47284acfa33cff041d5
wang yan <wangyan@vmware.com>
2019-06-18 14:52:53 +0800

    fix issues

--- src/core/middlewares/regquota/handler.go
+++ src/core/middlewares/regquota/handler.go
@@ -1,8 +1,12 @@
 package regquota
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/docker/distribution"
+	"github.com/goharbor/harbor/src/common/utils/log"
 	"github.com/goharbor/harbor/src/core/middlewares/util"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -20,34 +24,32 @@ func New(next http.Handler) (http.Handler, error) {
 func (bh regQuotaHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	match, _, _ := util.MatchManifestURL(req)
 	if match {
-		//var imageSize int64
-		//var imageDigest string
-		//mediaType := req.Header.Get("Content-Type")
-		//if req.Method == http.MethodPut && mediaType == "application/vnd.docker.distribution.manifest.v2+json" {
-		//	data, err := ioutil.ReadAll(req.Body)
-		//	if err != nil {
-		//		log.Warningf("Error occured when to copy manifest body %v", err)
-		//		http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to decode manifest body %v", err)), http.StatusInternalServerError)
-		//		return
-		//	}
-		//
-		//	manifest, desc, err := distribution.UnmarshalManifest(mediaType, data)
-		//	if err != nil {
-		//		log.Warningf("Error occured when to Unmarshal Manifest %v", err)
-		//		http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to Unmarshal Manifest %v", err)), http.StatusInternalServerError)
-		//		return
-		//	}
-		//	for _, ref := range manifest.References() {
-		//		imageSize += ref.Size
-		//	}
-		//	imageDigest = desc.Digest.String()
-		//	imageSize += desc.Size
-		//	log.Infof("manifest digest... %s", imageDigest)
-		//	log.Infof("manifest size... %v", imageSize)
-		//	req.Body = ioutil.NopCloser(bytes.NewBuffer(data))
-		//}
-		http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to Unmarshal Manifest")), http.StatusInternalServerError)
-		return
+		var imageSize int64
+		var imageDigest string
+		mediaType := req.Header.Get("Content-Type")
+		if req.Method == http.MethodPut && mediaType == "application/vnd.docker.distribution.manifest.v2+json" {
+			data, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Warningf("Error occured when to copy manifest body %v", err)
+				http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to decode manifest body %v", err)), http.StatusInternalServerError)
+				return
+			}
+
+			manifest, desc, err := distribution.UnmarshalManifest(mediaType, data)
+			if err != nil {
+				log.Warningf("Error occured when to Unmarshal Manifest %v", err)
+				http.Error(rw, util.MarshalError("InternalServerError", fmt.Sprintf("Error occured when to Unmarshal Manifest %v", err)), http.StatusInternalServerError)
+				return
+			}
+			for _, ref := range manifest.References() {
+				imageSize += ref.Size
+			}
+			imageDigest = desc.Digest.String()
+			imageSize += desc.Size
+			log.Infof("manifest digest... %s", imageDigest)
+			log.Infof("manifest size... %v", imageSize)
+			req.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+		}
 	}
 
 	bh.next.ServeHTTP(rw, req)
