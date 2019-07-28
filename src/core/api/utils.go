29100d7ab33a8765e98959506903326308d35441
wang yan <wangyan@vmware.com>
2019-07-28 16:12:04 +0800

    fix issues

--- src/core/api/utils.go
+++ src/core/api/utils.go
@@ -17,6 +17,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"sort"
 	"strings"
 
@@ -246,10 +247,11 @@ func initRegistryClient() (r *registry.Registry, err error) {
 	if err := utils.TestTCPConn(addr, 60, 2); err != nil {
 		return nil, err
 	}
+	rw := httptest.NewRecorder()
 
 	authorizer := auth.NewRawTokenAuthorizer("harbor-core", token.Registry)
 	return registry.NewRegistry(endpoint, &http.Client{
-		Transport: registry.NewTransport(registry.GetHTTPTransport(), authorizer),
+		Transport: registry.NewTransport(registry.GetHTTPTransport(), rw, authorizer),
 	})
 }
 
