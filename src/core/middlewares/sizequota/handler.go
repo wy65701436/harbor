0dc2c2e4f663bd103d3a48546f1bda10df0cebf1
wang yan <wangyan@vmware.com>
2019-08-22 18:27:52 +0800

    update

--- src/core/middlewares/sizequota/handler.go
+++ src/core/middlewares/sizequota/handler.go
@@ -59,7 +59,7 @@ func (h *sizeQuotaHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 		log.Warningf("Error occurred when to handle request in size quota handler: %v", err)
 		//http.Error(rw, util.MarshalError("InternalError", fmt.Sprintf("Error occurred when to handle request in size quota handler: %v", err)),
 		//	http.StatusInternalServerError)
-		http.Error(rw, util.MarshalError("UNAUTHORIZED", "Out of quota...."), http.StatusUnauthorized)
+		http.Error(rw, util.MarshalError("UNSUPPORTED", "Out of quota...."), http.StatusMethodNotAllowed)
 		return
 	}
 
