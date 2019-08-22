187a8bc7f8806ddf90beed65b9c5d70428dd36c9
wang yan <wangyan@vmware.com>
2019-08-22 18:00:52 +0800

    try error code

--- src/core/middlewares/sizequota/handler.go
+++ src/core/middlewares/sizequota/handler.go
@@ -57,8 +57,9 @@ func (h *sizeQuotaHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 
 	if err := interceptor.HandleRequest(req); err != nil {
 		log.Warningf("Error occurred when to handle request in size quota handler: %v", err)
-		http.Error(rw, util.MarshalError("InternalError", fmt.Sprintf("Error occurred when to handle request in size quota handler: %v", err)),
-			http.StatusInternalServerError)
+		//http.Error(rw, util.MarshalError("InternalError", fmt.Sprintf("Error occurred when to handle request in size quota handler: %v", err)),
+		//	http.StatusInternalServerError)
+		http.Error(rw, util.MarshalError("NAME_UNKNOWN", "Out of quota...."), http.StatusNotFound)
 		return
 	}
 
