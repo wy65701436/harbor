d161faed04842c20620f73c6538bb9671791c6fc
wang yan <wangyan@vmware.com>
2019-08-30 19:16:08 +0800

    debug webhook

--- src/common/main.go
+++ src/common/main.go
@@ -1,5 +1,29 @@
-package main
+package common
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	$END$
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://10.160.218.91:8080", nil)
+	req.Close = true
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(content)[:100])
 }
