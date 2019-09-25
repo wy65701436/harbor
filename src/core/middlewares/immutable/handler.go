5cf235ef8021f2a6271b2d9120f493e70783a07c
wang yan <wangyan@vmware.com>
2019-09-25 17:11:20 +0800

    add immu matche code

---
+++ src/core/middlewares/immutable/handler.go
@@ -0,0 +1,68 @@
+// Copyright Project Harbor Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package immutable
+
+import (
+	"fmt"
+	"github.com/goharbor/harbor/src/core/middlewares/util"
+	"github.com/goharbor/harbor/src/pkg/art"
+	"github.com/goharbor/harbor/src/pkg/immutable/match/rule"
+	"net/http"
+)
+
+type immutableHandler struct {
+	next http.Handler
+}
+
+// New ...
+func New(next http.Handler) http.Handler {
+	return &immutableHandler{
+		next: next,
+	}
+}
+
+// ServeHTTP ...
+func (rh immutableHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if match, _, _ := util.MatchDeleteManifest(req); !match {
+		return
+	}
+	info, ok := util.ManifestInfoFromContext(req.Context())
+	if !ok {
+		var err error
+		info, err = util.ParseManifestInfoFromPath(req)
+		if err != nil {
+			return
+		}
+	}
+
+	var cands []*art.Candidate
+	c := &art.Candidate{
+		Repository: info.Repository,
+		Tag:        info.Tag,
+	}
+	cands = append(cands, c)
+	isImmutableTag, err := rule.NewRuleMatcher(info.ProjectID).Match(cands)
+	if err != nil {
+		return
+	}
+
+	if isImmutableTag {
+		http.Error(rw, util.MarshalError("DENIED",
+			fmt.Sprintf("The tag:%s:%s is immutable, cannot be overwrite.", info.Repository, info.Tag)), http.StatusForbidden)
+		return
+	}
+
+	rh.next.ServeHTTP(rw, req)
+}
---
+++ src/pkg/immutable/match/rule/matcher.go
@@ -0,0 +1,85 @@
+package rule
+
+import (
+	"github.com/goharbor/harbor/src/pkg/art"
+	"github.com/goharbor/harbor/src/pkg/art/selectors/index"
+	"github.com/goharbor/harbor/src/pkg/immutable"
+	"github.com/goharbor/harbor/src/pkg/immutable/rule"
+
+	"encoding/json"
+)
+
+// RuleMatcher ...
+type RuleMatcher struct {
+	pid   int64
+	rules []rule.Metadata
+}
+
+// Match ...
+func (rm *RuleMatcher) Match(uploads []*art.Candidate) (bool, error) {
+	for _, r := range rm.rules {
+		if r.Disabled {
+			continue
+		}
+
+		// match repositories according to the repository selectors
+		var repositoryCandidates []*art.Candidate
+		for _, repositorySelector := range r.RepoSelectors["repository"] {
+			selector, err := index.Get(repositorySelector.Kind, repositorySelector.Decoration,
+				repositorySelector.Pattern)
+			if err != nil {
+				return false, err
+			}
+			repositoryCandidates, err = selector.Select(uploads)
+			if err != nil {
+				return false, err
+			}
+		}
+
+		if len(repositoryCandidates) == 0 {
+			continue
+		}
+
+		// match tag according to the tag selectors
+		var tagCandidates []*art.Candidate
+		for _, tagSelector := range r.TagSelectors {
+			selector, err := index.Get(tagSelector.Kind, tagSelector.Decoration,
+				tagSelector.Pattern)
+			if err != nil {
+				return false, err
+			}
+			tagCandidates, err = selector.Select(uploads)
+			if err != nil {
+				return false, err
+			}
+		}
+
+		if len(tagCandidates) == 0 {
+			continue
+		}
+		return true, nil
+	}
+	return false, nil
+}
+
+func (rm *RuleMatcher) getImmutableRules() error {
+	rules, err := immutable.NewDefaultRuleManager().QueryEnabledImmutableRuleByProjectID(rm.pid)
+	if err != nil {
+		return err
+	}
+	for _, r := range rules {
+		var rmeta rule.Metadata
+		if err := json.Unmarshal([]byte(r.TagFilter), rmeta); err != nil {
+			return err
+		}
+		rm.rules = append(rm.rules, rmeta)
+	}
+	return nil
+}
+
+// NewRuleMatcher ...
+func NewRuleMatcher(pid int64) RuleMatcher {
+	return RuleMatcher{
+		pid: pid,
+	}
+}
