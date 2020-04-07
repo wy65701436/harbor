53e17c4a54682f0cc7c1f84f9cde0c933e1e11cf
wang yan <wangyan@vmware.com>
2020-04-07 18:01:58 +0800

    fix

--- src/server/middleware/csrf/csrf.go
+++ src/server/middleware/csrf/csrf.go
@@ -45,7 +45,7 @@ func attachToken(w http.ResponseWriter, r *http.Request) {
 
 func handleError(w http.ResponseWriter, r *http.Request) {
 	attachToken(w, r)
-	serror.SendError(w, errors.New(csrf.FailureReason(r)).WithCode(errors.ForbiddenCode))
+	serror.SendError(w, errors.New(csrf.FailureReason(r)).WithCode(errors.ForbiddenCode).WithMessage("csrf failure"))
 	return
 }
 
