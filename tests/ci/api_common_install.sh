523a6c282f622d254934266bb98ee729ba96ebb6
wang yan <wangyan@vmware.com>
2020-07-14 16:19:41 +0800

    fix api gc

--- tests/ci/api_common_install.sh
+++ tests/ci/api_common_install.sh
@@ -30,7 +30,9 @@ if [ $GITHUB_TOKEN ];
 then
     sed "s/# github_token: xxx/github_token: $GITHUB_TOKEN/" -i make/harbor.yml
 fi
-sudo make install COMPILETAG=compile_golangimage GOBUILDTAGS="include_oss include_gcs" NOTARYFLAG=true CLAIRFLAG=true TRIVYFLAG=true CHARTFLAG=true GEN_TLS=true
+sudo make compile build prepare COMPILETAG=compile_golangimage GOBUILDTAGS="include_oss include_gcs" NOTARYFLAG=true CLAIRFLAG=true TRIVYFLAG=true CHARTFLAG=true GEN_TLS=true
+sudo echo $'\nUTTEST=true' >> ./make/common/config/jobservice/env
+sudo make start
 
 # waiting 5 minutes to start
 for((i=1;i<=30;i++)); do
