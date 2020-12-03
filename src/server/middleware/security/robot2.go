0b73a83293dd2c0a35e5b68a9a0729b173b84cc7
Wang Yan <wangyan@vmware.com>
2020-12-03 18:13:06 +0800

    fix robot name conflict issue
    add the project name into the robot account name pattern
    
    Signed-off-by: wang yan <wangyan@vmware.com>

--- src/server/middleware/security/robot2.go
+++ src/server/middleware/security/robot2.go
@@ -5,43 +5,58 @@ import (
 	"github.com/goharbor/harbor/src/common/security"
 	robotCtx "github.com/goharbor/harbor/src/common/security/robot"
 	"github.com/goharbor/harbor/src/common/utils"
+	"github.com/goharbor/harbor/src/controller/project"
 	robot_ctl "github.com/goharbor/harbor/src/controller/robot"
 	"github.com/goharbor/harbor/src/core/config"
 	"github.com/goharbor/harbor/src/lib/log"
 	"github.com/goharbor/harbor/src/lib/q"
 	"github.com/goharbor/harbor/src/pkg/permission/types"
+	"regexp"
+	"strings"
+	"time"
 
 	"github.com/goharbor/harbor/src/pkg/robot2/model"
 	"net/http"
-	"strings"
 )
 
 type robot2 struct{}
 
 func (r *robot2) Generate(req *http.Request) security.Context {
 	log := log.G(req.Context())
+	var ok bool
 	name, secret, ok := req.BasicAuth()
 	if !ok {
 		return nil
 	}
-	if !strings.HasPrefix(name, config.RobotPrefix()) {
-		return nil
-	}
-	key, err := config.SecretKey()
-	if err != nil {
-		log.Error("failed to get secret key")
-		return nil
-	}
-	_, err = utils.ReversibleDecrypt(secret, key)
-	if err != nil {
-		log.Errorf("failed to decode secret key: %s, %v", secret, err)
-		return nil
+
+	var projectName string
+	var isProjectLevelRobot bool
+	var robotName string
+	robotName, ok = matchSysRobotPattern(name)
+	if !ok {
+		projectName, robotName, ok = matchProRobotPattern(name)
+		if !ok {
+			return nil
+		}
+		isProjectLevelRobot = true
 	}
 
-	// TODO use the naming pattern to avoid the permission boundary crossing.
-	robots, err := robot_ctl.Ctl.List(req.Context(), q.New(q.KeyWords{
-		"name": strings.TrimPrefix(name, config.RobotPrefix()),
-	}), &robot_ctl.Option{
+	query := &q.Query{}
+	query.Keywords = q.KeyWords{
+		"Name": robotName,
+	}
+	if isProjectLevelRobot {
+		pro, err := project.Ctl.Get(req.Context(), projectName)
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
+		query.Keywords = q.KeyWords{
+			"Name":      robotName,
+			"ProjectID": pro.ProjectID,
+		}
+	}
+	robots, err := robot_ctl.Ctl.List(req.Context(), query, &robot_ctl.Option{
 		WithPermission: true,
 	})
 	if err != nil {
@@ -49,21 +64,30 @@ func (r *robot2) Generate(req *http.Request) security.Context {
 		return nil
 	}
 	if len(robots) == 0 {
+		log.Errorf("no robot found: %s", name)
+		return nil
+	}
+	if len(robots) > 1 {
+		log.Errorf("multiple robot entries found: %s", name)
+		return nil
+	}
+
+	robot := robots[0]
+	if utils.Encrypt(secret, robot.Salt, utils.SHA256) != robot.Secret {
+		log.Errorf("failed to authenticate robot account: %s", name)
+		return nil
+	}
+	if robot.Disabled {
+		log.Errorf("failed to authenticate disabled robot account: %s", name)
+		return nil
+	}
+	now := time.Now().Unix()
+	if robot.ExpiresAt != -1 && robot.ExpiresAt <= now {
+		log.Errorf("the robot account is expirated: %s", name)
 		return nil
 	}
 
 	var accesses []*types.Policy
-	robot := robots[0]
-	if secret != robot.Secret {
-		log.Error("the secret provided is not correct.")
-		return nil
-	}
-	if robot.Disabled {
-		log.Error("the robot is disabled.")
-		return nil
-	}
-	// add the expiration check
-
 	for _, p := range robot.Permissions {
 		for _, a := range p.Access {
 			accesses = append(accesses, &types.Policy{
@@ -75,8 +99,30 @@ func (r *robot2) Generate(req *http.Request) security.Context {
 	}
 
 	modelRobot := &model.Robot{
-		Name: strings.TrimPrefix(name, config.RobotPrefix()),
+		Name: robotName,
 	}
 	log.Infof("a robot2 security context generated for request %s %s", req.Method, req.URL.Path)
 	return robotCtx.NewSecurityContext(modelRobot, robot.Level == robot_ctl.LEVELSYSTEM, accesses)
+}
+
+// matchSysRobotPattern match the name with system robot pattern and return the raw name
+func matchSysRobotPattern(name string) (robot string, match bool) {
+	name = strings.TrimPrefix(name, config.RobotPrefix())
+	sysRobotNameReg := fmt.Sprintf("^(?P<robot>[a-z0-9]+(?:[._-][a-z0-9]+)*)$")
+	strs := regexp.MustCompile(sysRobotNameReg).FindStringSubmatch(name)
+	if len(strs) < 2 {
+		return "", false
+	}
+	return strs[1], true
+}
+
+// matchProRobotPattern match the name with project robot pattern and return the project and raw robot name
+func matchProRobotPattern(name string) (project, robot string, match bool) {
+	name = strings.TrimPrefix(name, config.RobotPrefix())
+	proRobotNameReg := fmt.Sprintf("^%s\\+%s", `(?P<project>[a-z0-9]+(?:[._-][a-z0-9]+)*)`, `(?P<robot>[a-z0-9]+(?:[._-][a-z0-9]+)*)$`)
+	strs := regexp.MustCompile(proRobotNameReg).FindStringSubmatch(name)
+	if len(strs) < 3 {
+		return "", "", false
+	}
+	return strs[1], strs[2], true
 }
--- src/server/middleware/security/robot2_test.go
+++ src/server/middleware/security/robot2_test.go
@@ -2,24 +2,16 @@ package security
 
 import (
 	"github.com/goharbor/harbor/src/common"
-	"github.com/goharbor/harbor/src/common/utils/test"
 	core_cfg "github.com/goharbor/harbor/src/core/config"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
-	"os"
 	"testing"
 )
 
 func TestRobot2(t *testing.T) {
-	secretKeyPath := "/tmp/secretkey"
-	_, err := test.GenerateKey(secretKeyPath)
-	assert.Nil(t, err)
-	defer os.Remove(secretKeyPath)
-	os.Setenv("KEY_PATH", secretKeyPath)
-
 	conf := map[string]interface{}{
-		common.RobotPrefix: "robot@",
+		common.RobotNamePrefix: "robot@",
 	}
 	core_cfg.InitWithSettings(conf)
 
@@ -29,4 +21,72 @@ func TestRobot2(t *testing.T) {
 	req.SetBasicAuth("robot@est1", "Harbor12345")
 	ctx := robot.Generate(req)
 	assert.Nil(t, ctx)
+}
+
+func TestMatchSysRobotPattern(t *testing.T) {
+	conf := map[string]interface{}{
+		common.RobotNamePrefix: "robot$",
+	}
+	core_cfg.InitWithSettings(conf)
+
+	r, ok := matchSysRobotPattern("robot$test1")
+	assert.True(t, ok)
+	assert.Equal(t, "test1", r)
+
+	r, ok = matchSysRobotPattern("robot$q1w2e3r4")
+	assert.True(t, ok)
+	assert.Equal(t, "q1w2e3r4", r)
+
+	r, ok = matchSysRobotPattern("robot@test1")
+	assert.False(t, ok)
+
+	conf = map[string]interface{}{
+		common.RobotNamePrefix: "mysettting&",
+	}
+	core_cfg.InitWithSettings(conf)
+
+	r, ok = matchSysRobotPattern("robot$q1w2e3r4")
+	assert.False(t, ok)
+
+	r, ok = matchSysRobotPattern("mysettting&test122")
+	assert.True(t, ok)
+	assert.Equal(t, "test122", r)
+}
+
+func TestMatchProRobotPattern(t *testing.T) {
+	conf := map[string]interface{}{
+		common.RobotNamePrefix: "robot$",
+	}
+	core_cfg.InitWithSettings(conf)
+
+	p, r, ok := matchProRobotPattern("robot$library+test1")
+	assert.True(t, ok)
+	assert.Equal(t, "library", p)
+	assert.Equal(t, "test1", r)
+
+	p, r, ok = matchProRobotPattern("robot$harbor.test+robot.test")
+	assert.True(t, ok)
+	assert.Equal(t, "harbor.test", p)
+	assert.Equal(t, "robot.test", r)
+
+	p, r, ok = matchProRobotPattern("robot$harbor_test+robot_test.account")
+	assert.True(t, ok)
+	assert.Equal(t, "harbor_test", p)
+	assert.Equal(t, "robot_test.account", r)
+
+	_, _, ok = matchProRobotPattern("robot@library+test1")
+	assert.False(t, ok)
+
+	conf = map[string]interface{}{
+		common.RobotNamePrefix: "mysetting!@#$",
+	}
+	core_cfg.InitWithSettings(conf)
+
+	_, _, ok = matchProRobotPattern("robot@library+test1")
+	assert.False(t, ok)
+
+	p, r, ok = matchProRobotPattern("mysetting!@#$harbor_test+robot_test.account")
+	assert.True(t, ok)
+	assert.Equal(t, "harbor_test", p)
+	assert.Equal(t, "robot_test.account", r)
 }
