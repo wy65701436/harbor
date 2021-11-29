a6fd1b01628fee5e7f33514bfab224dada0c4178
Wang Yan <wangyan@vmware.com>
2021-11-29 13:12:05 +0800

    add cosign middleware
    the middleware is for cosign signature to land accessory data.
    
    Signed-off-by: Wang Yan <wangyan@vmware.com>

--- src/server/middleware/cosign/cosign.go
+++ src/server/middleware/cosign/cosign.go
@@ -2,29 +2,53 @@ package cosign
 
 import (
 	"fmt"
-	"github.com/docker/distribution/manifest/manifestlist"
-	"github.com/docker/distribution/manifest/schema1"
-	"github.com/docker/distribution/manifest/schema2"
 	"github.com/docker/distribution/reference"
+	"github.com/goharbor/harbor/src/controller/artifact"
 	"github.com/goharbor/harbor/src/lib"
 	"github.com/goharbor/harbor/src/lib/errors"
 	"github.com/goharbor/harbor/src/lib/log"
+	"github.com/goharbor/harbor/src/pkg/accessory"
+	"github.com/goharbor/harbor/src/pkg/accessory/model"
+	"github.com/goharbor/harbor/src/pkg/accessory/model/base"
+	"github.com/goharbor/harbor/src/pkg/accessory/model/cosign"
 	"github.com/goharbor/harbor/src/pkg/distribution"
 	"github.com/goharbor/harbor/src/server/middleware"
-	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"io/ioutil"
 	"net/http"
 	"regexp"
 )
 
 var (
-	cosignReg = regexp.MustCompile(fmt.Sprintf(`sha256-%s.sig$`, reference.IdentifierRegexp))
+	cosignDigest = "digest"
+	// repositorySubexp is the name for sub regex that maps to repository name in the url
+	repositorySubexp     = "repository"
+	cosignRe             = regexp.MustCompile(fmt.Sprintf(`^/v2/(?P<%s>%s)/manifests/sha256-(?P<%s>%s).sig$`, repositorySubexp, reference.NameRegexp.String(), cosignDigest, reference.IdentifierRegexp))
+	mediaTypeCosignLayer = "application/vnd.dev.cosign.simplesigning.v1+json"
 )
 
 // CosignSignatureMiddleware middleware to record the linkeage of artifact and its accessory
+/* PUT /v2/library/hello-world/manifests/sha256-1b26826f602946860c279fce658f31050cff2c596583af237d971f4629b57792.sig
+{
+	"schemaVersion":2,
+	"config":{
+		"mediaType":"application/vnd.oci.image.config.v1+json",
+		"size":233,
+		"digest":"sha256:d4e6059ece7bea95266fd7766353130d4bf3dc21048b8a9783c98b8412618c38"
+	},
+	"layers":[
+		{
+			"mediaType":"application/vnd.dev.cosign.simplesigning.v1+json",
+			"size":250,
+			"digest":"sha256:91a821a0e2412f1b99b07bfe176451bcc343568b761388718abbf38076048564",
+			"annotations":{
+				"dev.cosignproject.cosign/signature":"MEUCIQD/imXjZJlcV82eXu9y9FJGgbDwVPw7AaGFzqva8G+CgwIgYc4CRvEjwoAwkzGoX+aZxQWCASpv5G+EAWDKOJRLbTQ="
+			}
+		}
+	]
+}
+*/
 func CosignSignatureMiddleware() func(http.Handler) http.Handler {
 	return middleware.AfterResponse(func(w http.ResponseWriter, r *http.Request, statusCode int) error {
-		// Only record when patch blob upload success
 		if statusCode != http.StatusAccepted {
 			return nil
 		}
@@ -37,9 +61,13 @@ func CosignSignatureMiddleware() func(http.Handler) http.Handler {
 		if info == none {
 			return errors.New("artifactinfo middleware required before this middleware").WithCode(errors.NotFoundCode)
 		}
+		if info.Tag == "" {
+			return nil
+		}
 
 		// Needs tag to match the cosign tag pattern.
-		if info.Tag == "" {
+		_, subjectArtDigest, ok := matchCosignSignaturePattern(r.URL.Path)
+		if !ok {
 			return nil
 		}
 
@@ -49,47 +77,53 @@ func CosignSignatureMiddleware() func(http.Handler) http.Handler {
 		}
 
 		contentType := r.Header.Get("Content-Type")
-		manifest, descriptor, err := distribution.UnmarshalManifest(contentType, body)
+		manifest, desc, err := distribution.UnmarshalManifest(contentType, body)
 		if err != nil {
 			logger.Errorf("unmarshal manifest failed, error: %v", err)
 			return err
 		}
 
+		var hasSignature bool
 		for _, descriptor := range manifest.References() {
-			digest := descriptor.Digest.String()
-			switch descriptor.MediaType {
-			// skip foreign layer
-			case schema2.MediaTypeForeignLayer:
-				continue
-			// manifest or index
-			case v1.MediaTypeImageIndex, manifestlist.MediaTypeManifestList,
-				v1.MediaTypeImageManifest, schema2.MediaTypeManifest,
-				schema1.MediaTypeSignedManifest, schema1.MediaTypeManifest:
-				if err = c.Copy(srcRepo, digest, dstRepo, digest, false); err != nil {
-					return err
-				}
-			// common layer
-			default:
-				exist, err := c.BlobExist(dstRepo, digest)
-				if err != nil {
-					return err
-				}
-				// the layer already exist, skip
-				if exist {
-					continue
-				}
-				// when the copy happens inside the same registry, use mount
-				if err = c.MountBlob(srcRepo, digest, dstRepo); err != nil {
-					return err
-				}
+			if descriptor.MediaType == mediaTypeCosignLayer {
+				hasSignature = true
+				break
 			}
 		}
 
-		mediaType, payload, err := manifest.Payload()
-		if err != nil {
-			return err
+		if hasSignature {
+			subjectArt, err := artifact.Ctl.GetByReference(ctx, info.Repository, fmt.Sprintf("sha256:%s", subjectArtDigest), nil)
+			if err != nil {
+				return err
+			}
+			art, err := artifact.Ctl.GetByReference(ctx, info.Repository, desc.Digest.String(), nil)
+			if err != nil {
+				return err
+			}
+
+			_, err = accessory.Mgr.Create(ctx, &cosign.Signature{base.Default{
+				Opts: &model.Options{
+					ArtifactID:        art.ID,
+					SubjectArtifactID: subjectArt.ID,
+					Size:              desc.Size,
+					Digest:            desc.Digest.String(),
+				},
+			}})
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
 	})
+}
+
+// matchCosignSignaturePattern checks whether the provided path matches the blob upload URL pattern,
+// if does, returns the repository as well
+func matchCosignSignaturePattern(path string) (repository, digest string, match bool) {
+	strs := cosignRe.FindStringSubmatch(path)
+	if len(strs) < 3 {
+		return "", "", false
+	}
+	return strs[1], strs[2], true
 }
